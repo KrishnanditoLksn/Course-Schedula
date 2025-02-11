0f866b3355145cc9902bbaab6209eb6e4c581a71
KrishnanditoLksn <krislaksana123@gmail.com>
2025-02-12 05:54:16 +0700

    feat:added bind to show list of course item

--- app/src/main/java/com/dicoding/courseschedule/paging/CourseViewHolder.kt
+++ app/src/main/java/com/dicoding/courseschedule/paging/CourseViewHolder.kt
@@ -1,15 +1,19 @@
 package com.dicoding.courseschedule.paging
 
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.dicoding.courseschedule.R
 import com.dicoding.courseschedule.data.Course
 import com.dicoding.courseschedule.util.DayName.Companion.getByNumber
 
-class CourseViewHolder(view: View): RecyclerView.ViewHolder(view) {
+class CourseViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
     private lateinit var course: Course
     private val timeString = itemView.context.resources.getString(R.string.time_format)
+    private val tvTittle = itemView.findViewById<TextView>(R.id.tv_course)
+    private val tvTime = itemView.findViewById<TextView>(R.id.tv_time)
+    private val tvLecturer = itemView.findViewById<TextView>(R.id.tv_lecturer)
 
     //TODO 7 : Complete ViewHolder to show item
     fun bind(course: Course, clickListener: (Course) -> Unit) {
@@ -18,8 +22,9 @@ class CourseViewHolder(view: View): RecyclerView.ViewHolder(view) {
         course.apply {
             val dayName = getByNumber(day)
             val timeFormat = String.format(timeString, dayName, startTime, endTime)
-
-
+            tvTittle.text = course.courseName
+            tvTime.text = timeFormat
+            tvLecturer.text = course.lecturer
         }
 
         itemView.setOnClickListener {

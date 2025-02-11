337d9f874ca4752dc012b5918bda95b9d72fc25d
KrishnanditoLksn <krislaksana123@gmail.com>
2025-02-11 21:35:13 +0700

    feat:all business logic for course app

--- app/src/main/java/com/dicoding/courseschedule/data/DataRepository.kt
+++ app/src/main/java/com/dicoding/courseschedule/data/DataRepository.kt
@@ -2,36 +2,51 @@ package com.dicoding.courseschedule.data
 
 import android.content.Context
 import androidx.lifecycle.LiveData
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
 import androidx.paging.PagingData
+import androidx.paging.liveData
 import com.dicoding.courseschedule.util.QueryType
+import com.dicoding.courseschedule.util.QueryUtil
 import com.dicoding.courseschedule.util.SortType
 import com.dicoding.courseschedule.util.executeThread
+import java.util.Calendar
 
 //TODO 4 : Implement repository with appropriate dao
 class DataRepository(private val dao: CourseDao) {
 
-    fun getNearestSchedule(queryType: QueryType) : LiveData<Course?> {
-        throw NotImplementedError("needs implementation")
+    fun getNearestSchedule(queryType: QueryType): LiveData<Course?> {
+        val queryUtil = QueryUtil.nearestQuery(queryType)
+        return dao.getNearestSchedule(queryUtil)
     }
 
     fun getAllCourse(sortType: SortType): LiveData<PagingData<Course>> {
-        throw NotImplementedError("needs implementation")
+        val sortUtil = QueryUtil.sortedQuery(sortType)
+        return Pager(
+            config = PagingConfig(
+                pageSize = PAGE_SIZE,
+                enablePlaceholders = true
+            ),
+            pagingSourceFactory = { dao.getAll(sortUtil) }
+        ).liveData
     }
 
-    fun getCourse(id: Int) : LiveData<Course> {
-        throw NotImplementedError("needs implementation")
+    fun getCourse(id: Int): LiveData<Course> {
+        return dao.getCourse(id)
     }
 
-    fun getTodaySchedule() : List<Course> {
-        throw NotImplementedError("needs implementation")
+    fun getTodaySchedule(): List<Course> {
+        val calender = Calendar.getInstance()
+        val today = calender.get(Calendar.DAY_OF_WEEK)
+        return dao.getTodaySchedule(today)
     }
 
     fun insert(course: Course) = executeThread {
-
+        dao.insert(course)
     }
 
     fun delete(course: Course) = executeThread {
-
+        dao.delete(course)
     }
 
     companion object {

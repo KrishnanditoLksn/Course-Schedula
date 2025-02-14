3d4f76f227ab21ecc9e9b95d99574e85a2f2f10b
KrishnanditoLksn <krislaksana123@gmail.com>
2025-02-14 07:39:04 +0700

    feat:implement dark mode and daily reminder using Alarm Manager

--- app/src/main/java/com/dicoding/courseschedule/ui/setting/SettingsFragment.kt
+++ app/src/main/java/com/dicoding/courseschedule/ui/setting/SettingsFragment.kt
@@ -2,15 +2,42 @@ package com.dicoding.courseschedule.ui.setting
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreference
 import com.dicoding.courseschedule.R
+import com.dicoding.courseschedule.notification.DailyReminder
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
         //TODO 10 : Update theme based on value in ListPreference
+        val themePref = findPreference<ListPreference>(getString(R.string.pref_key_dark))
+
+        themePref?.setOnPreferenceChangeListener { _, newValue ->
+            val value = newValue as String
+            val themeMode = when (value) {
+                getString(R.string.pref_dark_off) -> AppCompatDelegate.MODE_NIGHT_NO
+                getString(R.string.pref_dark_on) -> AppCompatDelegate.MODE_NIGHT_YES
+                else -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+            }
+            updateTheme(themeMode)
+        }
+
+
         //TODO 11 : Schedule and cancel notification in DailyReminder based on SwitchPreference
+        val prefAlarm = findPreference<SwitchPreference>(getString(R.string.pref_key_notify))
+
+        prefAlarm?.setOnPreferenceChangeListener { _, newValue ->
+            val dailyReminder = DailyReminder()
+            if (newValue as Boolean) {
+                dailyReminder.setDailyReminder(requireContext())
+            } else {
+                dailyReminder.cancelAlarm(requireContext())
+            }
+            true
+        }
     }
 
     private fun updateTheme(nightMode: Int): Boolean {
